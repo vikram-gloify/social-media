f3258cd6d2bba5cc80b9e0ad6df14335ea341691
Vikram Singh <85120364+vikramsingh159@users.noreply.github.com>
2022-07-31 19:04:18 +0530

    media query add left side article

--- src/components/LeftCard/LeftCard.js
+++ src/components/LeftCard/LeftCard.js
@@ -1,54 +1,55 @@
 import React from 'react';
+// import Images Constant Images.js
 import { Avatar, calender, chat, cog, earth, feed, LeftBg, notification, person } from '../../constant/Images';
 
 const Card = () => {
     return (
 
         <>
-            <div className="card mt-3">
+            <div className="card mt-3" >
                 <div className="card-header" style={{
                     background: `url(${LeftBg})`, backgroundPosition: `center`, backgroundSize: `cover`, backgroundRepeat: `no-repeat`,
                 }}>
 
                 </div>
                 <div className="card-body">
-                    <div className="avatar ms-5 mb-3">
-                        <a href="/"><img className="avatar-profile rounded" src={Avatar} alt="profile-left-img" /></a>
+                    <div className="profile-img profile-img-lg ms-5 mb-3">
+                        <a href="/"><img className="profile-avatar rounded img-responsive" src={Avatar} alt="profile-left-img" /></a>
                     </div>
                     <h5 className="card-title text-head text-center">Sam Lanson</h5>
                     <p className="card-subtitle mb-2 fw-light text-center">Web Developer at Webestica</p>
                     <p className="mt-3 fw-light text-center">I'd love to change the world, but they won’t give me the source code.</p>
 
-                    <div className="hstack gap-2 gap-xl-3 justify-content-center">
+                    <div className="like-post-follower gap-2 gap-xl-3 justify-content-center">
 
                         <div>
-                            <h6 className="mb-0 ">256</h6>
-                            <small className="fw-light">Post</small>
+                            <h6 className="mb-0 text-center text-break">256</h6>
+                            <small className="fw-light text-break">Post</small>
                         </div>
 
-                        <div className="vr"></div>
+                        <div className="vr ms-2 me-2"></div>
 
                         <div>
-                            <h6 className="mb-0 ">2.5K</h6>
-                            <small className="fw-light">Followers</small>
+                            <h6 className="mb-0 text-center text-break">2.5K</h6>
+                            <small className="fw-light text-break">Followers</small>
                         </div>
 
-                        <div className="vr"></div>
+                        <div className="vr  ms-2 me-2"></div>
 
                         <div>
-                            <h6 className="mb-0 ">365</h6>
-                            <small className="fw-light">Following</small>
+                            <h6 className="mb-0 text-center text-break">365</h6>
+                            <small className="fw-light text-break">Following</small>
                         </div>
                     </div>
                     <hr />
                     <ul className="list-unstyled">
-                        <li> <img className="svg" src={feed} alt="feed" /> <a href="/" className="card-down  text-decoration-none">Feed</a></li>
-                        <li> <img className="svg" src={person} alt="connection" /> <a href="/" className="card-down  text-decoration-none">Connections</a></li>
-                        <li> <img className="svg" src={earth} alt="latest-news" /> <a href="/" className="card-down  text-decoration-none">Latest News</a></li>
-                        <li> <img className="svg" src={calender} alt="event" /> <a href="/" className="card-down  text-decoration-none">Events</a></li>
-                        <li> <img className="svg" src={chat} alt="group" /> <a href="/" className="card-down  text-decoration-none">Groups</a></li>
-                        <li> <img className="svg" src={notification} alt="notifications" /> <a href="/" className="card-down  text-decoration-none">Notifications</a></li>
-                        <li> <img className="svg" src={cog} alt="setting" /> <a href="/" className="card-down  text-decoration-none">Settings</a></li>
+                        <li> <img className="svg" src={feed} alt="feed" /> <a href="/" className="fw-text text-decoration-none">Feed</a></li>
+                        <li> <img className="svg" src={person} alt="connection" /> <a href="/" className="fw-text text-decoration-none">Connections</a></li>
+                        <li> <img className="svg" src={earth} alt="latest-news" /> <a href="/" className="fw-text text-decoration-none">Latest News</a></li>
+                        <li> <img className="svg" src={calender} alt="event" /> <a href="/" className="fw-text text-decoration-none">Events</a></li>
+                        <li> <img className="svg" src={chat} alt="group" /> <a href="/" className="fw-text text-decoration-none">Groups</a></li>
+                        <li> <img className="svg" src={notification} alt="notifications" /> <a href="/" className="fw-text text-decoration-none">Notifications</a></li>
+                        <li> <img className="svg" src={cog} alt="setting" /> <a href="/" className="fw-text text-decoration-none">Settings</a></li>
                     </ul>
                 </div>
                 <div className="card-footer text-center" style={{ backgroundColor: `white` }} >
@@ -56,26 +57,26 @@ const Card = () => {
                 </div>
             </div>
             <ul className="nav small mt-4 justify-content-center lh-1">
-                <li className="nav-link">
-                    <a href="/" className="card-down text-decoration-none">About</a>
+                <li className="nav-item">
+                    <a href="/" className="nav-link text-decoration-none">About</a>
                 </li>
-                <li className="nav-link">
-                    <a href="/" className="card-down text-decoration-none">Setting</a>
+                <li className="nav-item">
+                    <a href="/" className="nav-link text-decoration-none">Setting</a>
                 </li>
-                <li className="nav-link">
-                    <a href="/" className="card-down text-decoration-none">Support</a>
+                <li className="nav-item">
+                    <a href="/" className="nav-link text-decoration-none">Support</a>
                 </li>
-                <li className="nav-link">
-                    <a href="/" className="card-down text-decoration-none">Docs</a>
+                <li className="nav-item">
+                    <a href="/" className="nav-link text-decoration-none">Docs</a>
                 </li>
-                <li className="nav-link">
-                    <a href="/" className="card-down text-decoration-none">Help</a>
+                <li className="nav-item">
+                    <a href="/" className="nav-link text-decoration-none">Help</a>
                 </li>
-                <li className="nav-link">
-                    <a href="/" className="card-down text-decoration-none">Privacys & Terms</a>
+                <li className="nav-item">
+                    <a href="/" className="nav-link text-decoration-none">Privacys & Terms</a>
                 </li>
             </ul>
-            <p className="small text-center mt-1">©2022 <a className="text-body text-decoration-none" target="_blank" href="/"> Webestica </a></p>
+            <p className="small text-center text-website mt-1">©2022 <a className="text-body text-website text-decoration-none" href="/"> Webestica </a></p>
         </ >
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LeftSide from '../../components/LeftSide/LeftSide';
-import MainContent from '../../components/MainContent/MainContent';
-import RightSide from '../../components/RightSide/RightSide';
+// import MainContent from '../../components/MainContent/MainContent';
+// import RightSide from '../../components/RightSide/RightSide';
 
 
 
@@ -14,13 +14,13 @@ const Home = () => {
                 <div className="col-lg-3">
                     <LeftSide />
                 </div>
-                <div className="col-md-8 col-lg-6">
+                {/* <div className="col-md-8 col-lg-6">
                     <MainContent />
 
                 </div>
                 <div className="col-lg-3">
                     <RightSide />
-                </div>
+                </div> */}
 
             </div>
 
