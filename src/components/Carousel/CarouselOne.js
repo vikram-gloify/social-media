55fbdea88b49a6190ba0d75dbf28afd51d2493c0
Vikram Singh <85120364+vikramsingh159@users.noreply.github.com>
2022-08-02 10:38:05 +0530

    main content slider story change same margin and padding

--- src/components/Carousel/CarouselOne.js
+++ src/components/Carousel/CarouselOne.js
@@ -6,41 +6,40 @@ import { images1, images2, images3, images4, images5 } from "../../constant/Imag
 
 export default class MultipleItems extends Component {
     render() {
-        const settings = {
-            dots: false,
-            infinite: true,
+        var settings = {
             speed: 500,
-            slidesToShow: 3,
+            slidesToShow: 5,
             slidesToScroll: 1,
-            arrows: true
+            arrows: false,
+
         };
         return (
             <div>
 
                 <Slider {...settings}>
-                    <div className="w-100 ps-3 bg-white p-5 border-dash  rounded">
-                        <span className="align-item-center"><i className="fa-solid fa-plus align-content-center"></i><h6 className="slider-react-post align-content-center"> Post a Story </h6></span>
+                    <div className="slider-img bg-white ps-3 border-dash">
+                        <i className="fa-solid fa-plus align-center"></i><h6 className="slider-react-post align-center"> Post a Story </h6>
                     </div>
                     <div className="ps-3">
 
-                        <span><img src={images1} className="w-100 rounded" alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Judy Nguyen</h6></span>
+                        <img className="slider-img" src={images1} alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Judy Nguyen</h6>
                     </div>
                     <div className="ps-3">
 
-                        <span><img src={images2} className="w-100 rounded" alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Samuel Bishop</h6></span>
+                        <img className="slider-img" src={images2} alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Samuel Bishop</h6>
                     </div>
                     <div className="ps-3">
 
-                        <span><img src={images3} className="w-100 rounded" alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Carolyn Ortiz</h6></span>
+                        <img className="slider-img" src={images3} alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Carolyn Ortiz</h6>
                     </div>
                     <div className="ps-3">
 
-                        <span><img src={images4} className="w-100 rounded" alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Amanda Reed</h6></span>
+                        <img className="slider-img" src={images4} alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Amanda Reed</h6>
 
                     </div>
                     <div className="ps-3">
 
-                        <span><img src={images5} className="w-100 rounded" alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Lori Stevens</h6></span>
+                        <img className="slider-img" src={images5} alt="" /><h6 className="text-white style-text" id="slider-react-carousel">Lori Stevens</h6>
                     </div>
                 </Slider>
             </div>
